0240ba3aa5e56e3d180b050bf1a507965f39a735
trottafede <ftrotta18@gmail.com>
2021-10-23 02:11:04 -0300

    Patch funcionando

--- router.js
+++ router.js
@@ -63,30 +63,86 @@ router.get("/admin", (req, res) => {
     "SELECT * FROM `seederDB` ",
     function (error, blogs, fields) {
       if (error) throw error;
-      res.render("home", { blogs });
+      res.render("admin", { blogs });
     }
   );
 
   connection.end();
-
-  res.render("admin");
 });
 
-router.get("/admin/createArticle", (req, res) => {
-  res.render("createArticle");
+router.get("/admin/updateArticle/:id", (req, res) => {
+  let key = req.params.id;
+  let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+  connection.connect();
+
+  let singleBlog = connection.query(
+    "SELECT * FROM `seederDB` WHERE `id` = (?)",
+    [key],
+    function (error, data, fields) {
+      if (error) throw error;
+      const singleBlog = data[0];
+      res.render("updateArticle", { singleBlog });
+    }
+  );
+  connection.end();
+});
+
+router.post("/admin/updateArticle", (req, res) => {
+  const { id, title, content, author, image } = req.body;
+  if (
+    title.length <= 255 &&
+    title.length >= 5 &&
+    content.length <= 21844 &&
+    content.length >= 5 &&
+    author.length <= 255 &&
+    author.length >= 5 &&
+    image.length <= 21844 &&
+    image.length >= 5
+  ) {
+    let fecha = new Date();
+    let queryString =
+      "UPDATE seederDB SET title = ?, content = ?, author = ?, image = ?, createdDate = ? WHERE id = ?";
+    let dataArray = [title, content, author, image, fecha, id];
+
+    // create the connection to database
+    let connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+
+    connection.connect(function (err) {
+      if (err) throw err;
+    });
+
+    // simple query
+    connection.query(queryString, dataArray, function (err, results, fields) {
+      if (err) throw err;
+      res.redirect("/admin");
+    });
+    connection.end();
+  }
 });
 
 router.post("/admin/createArticle", (req, res) => {
   let { title, content, author, image } = req.body;
 
   if (
-    (title.length <= 255 && title.length >= 5 && content.length <= 21,
-    844 &&
-      content.length >= 5 &&
-      author.length <= 255 &&
-      author.length >= 5 &&
-      image.length <= 255 &&
-      image.length >= 5)
+    title.length <= 255 &&
+    title.length >= 5 &&
+    content.length <= 21844 &&
+    content.length >= 5 &&
+    author.length <= 255 &&
+    author.length >= 5 &&
+    image.length <= 21844 &&
+    image.length >= 5
   ) {
     let fecha = new Date();
     let queryString = `INSERT INTO seederDB (title, content, author, image, createdDate) VALUES (?,?,?,?,?)`;
@@ -107,7 +163,7 @@ router.post("/admin/createArticle", (req, res) => {
     // simple query
     connection.query(queryString, dataArray, function (err, results, fields) {
       if (err) throw err;
-      res.redirect("/");
+      res.redirect("/admin");
     });
     connection.end();
   }
