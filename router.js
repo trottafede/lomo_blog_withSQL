ca06c9318526094346703539a2ee4460a15b43cb
trottafede <ftrotta18@gmail.com>
2021-10-24 22:34:46 -0300

    Borre mysql2 y agregue env example

--- router.js
+++ router.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("mysql2");
 const nodemailer = require("nodemailer");
 const { Client } = require("pg");
 
